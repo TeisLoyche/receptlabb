dac0394b190ef2300fd0b8c74c543d71c31b0b0c
Oskar Gester <gester.oskar@gmail.com>
2024-01-19 11:32:49 +0100

    Recipe service update SAVE

--- ReceptLabb/src/main/java/com/example/ReceptLabb/services/RecipeService.java
+++ ReceptLabb/src/main/java/com/example/ReceptLabb/services/RecipeService.java
@@ -1,5 +1,17 @@
 package com.example.ReceptLabb.services;
 
+import com.example.ReceptLabb.models.Recipe;
+import com.example.ReceptLabb.repository.RecipeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class RecipeService {
+    @Autowired
+    RecipeRepository recipeRepository;
+
+    public Recipe createRecipe(Recipe recipe) {
+        return recipeRepository.save(recipe);
+    }
 
 }
