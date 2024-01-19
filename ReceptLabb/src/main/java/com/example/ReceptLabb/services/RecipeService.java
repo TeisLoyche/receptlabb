5d8caadec85418af7183ed1df1a974ada4f777a5
Mia Stefansdotter <stefansdotter@gmail.com>
2024-01-19 10:48:52 +0100

    RecipeService update

--- ReceptLabb/src/main/java/com/example/ReceptLabb/services/RecipeService.java
+++ ReceptLabb/src/main/java/com/example/ReceptLabb/services/RecipeService.java
@@ -1,5 +1,17 @@
 package com.example.ReceptLabb.services;
 
-public class RecipeService {
+import com.example.ReceptLabb.repository.RecipeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.ReceptLabb.models.Recipe;
+import com.example.ReceptLabb.repository.RecipeRepository;
 
+@Service
+public class RecipeService {
+    @Autowired
+    RecipeRepository recipeRepository;
+
+    public Recipe createRecipe(Recipe recipe) {
+        return recipeRepository.save(recipe);
+    }
 }
