f815a5d38e2e81a5b06dc3ea4cf60324bc10cf98
Teis Loyche <teisloyche@hotmail.com>
2024-01-19 09:30:55 +0100

    first commit!

---
+++ ReceptLabb/src/main/java/com/example/ReceptLabb/ReceptLabbApplication.java
@@ -0,0 +1,13 @@
+package com.example.ReceptLabb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceptLabbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceptLabbApplication.class, args);
+	}
+
+}
