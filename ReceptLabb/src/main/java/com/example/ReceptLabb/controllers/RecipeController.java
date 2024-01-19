91a2f84044f934602179c7bab01ffcc27ab0bbce
Mia Stefansdotter <stefansdotter@gmail.com>
2024-01-19 11:52:39 +0100

    huu

--- ReceptLabb/src/main/java/com/example/ReceptLabb/controllers/RecipeController.java
+++ ReceptLabb/src/main/java/com/example/ReceptLabb/controllers/RecipeController.java
@@ -3,9 +3,10 @@ package com.example.ReceptLabb.controllers;
 import com.example.ReceptLabb.models.Recipe;
 import com.example.ReceptLabb.services.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value="/api")
@@ -19,10 +20,11 @@ public class RecipeController {
         return recipeService.createRecipe(recipe);
     }
     // GET all recipes
+    /*
     @GetMapping("/recipes")
     public List<Recipe> getAllRecipes() {
         return recipeService.getAllRecipes();
-    }
+    }*/
 
 
 }
--- ReceptLabb/src/main/java/com/example/ReceptLabb/services/RecipeService.java
+++ ReceptLabb/src/main/java/com/example/ReceptLabb/services/RecipeService.java
@@ -4,20 +4,6 @@ import com.example.ReceptLabb.repository.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.ReceptLabb.models.Recipe;
-import com.example.ReceptLabb.repository.RecipeRepository;
-import com.example.ReceptLabb.models.Recipe;
-import com.example.ReceptLabb.repository.RecipeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
-public class RecipeService {
-    @Autowired
-    RecipeRepository recipeRepository;
-
-    public Recipe createRecipe(Recipe recipe) {
-        return recipeRepository.save(recipe);
-    }
 
 @Service
 public class RecipeService {
